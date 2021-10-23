43958729ec390109dc0eb613ecfc44b53a77d32c
Gary Chen <chenhhgary@gmail.com>
2021-10-23 13:30:24 +0800

    player can now move with log

---
+++ Unity_Multithreaded/Assets/Scripts/Log.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent.parent = this.transform;
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("Player enter trigger.");
+    //        other.transform.parent.parent = this.transform;
+    //    }
+    //}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent.parent = null;
+        }
+    }
+}
---
+++ Unity_Multithreaded/Assets/Scripts/LogManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogManager : MonoBehaviour
+{
+    public Transform log;
+
+    private void Update()
+    {
+        Vector3 pos = new Vector3(log.position.x, log.position.y, log.position.z + Time.deltaTime * 5);
+        log.transform.position = pos;
+    }
+}
